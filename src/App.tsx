3869a07efc48fffb9298a1203faf3eebdc161a3c
ednsoares <ednei5137@gmail.com>
2024-02-23 02:02:19 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Contact} from '@/components/layout/Navibar_contact'
 import {Menu} from '@/components/layout/Navibar_menu'
+import Footer from './components/layout/footer'
 export function App() {
   return (
     <div>
       <Contact/>
       <Menu/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,63 @@
+import { Logo3 } from "./logo_footer";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container bg-black text-white p-4 mx-auto flex flex-col lg:flex-row justify-between items-center">
+                <div className="mb-4 lg:mb-0 lg:w-1/2 text-center flex flex-col items-center">
+                    <Logo3></Logo3>
+                    <p className="p-4">
+                        Em um elegante salão de beleza, a expertise se encontra com o luxo. Nossos talentosos estilistas estão prontos
+                        para transformar seu visual, proporcionando cortes e penteados que refletem sua personalidade única. Experimente
+                        o glamour e o cuidado personalizado que só um verdadeiro santuário de beleza pode oferecer.
+                    </p>
+                </div>
+                <div className="lg:w-1/2 flex justify-between">
+                    <div></div>
+                    <div>
+                        <h1 className="text-amber-800" >Empresa</h1>
+                        <br />
+                        <p>Sobre Nós</p>
+                        <p>Serviços</p>
+                        <p>Profissionais</p>
+                        <p>Trabalhe Conosco</p>
+                    </div>
+                    <div>
+                        <h1 className="text-amber-800" >Contato</h1>
+                        <br />
+                        <p>Lavada Especial</p>
+                        <p>Corte Feminino</p>
+                        <p>Escova</p>
+                        <p>Luzes</p>
+                        <p>Mais Serviços</p>
+                    </div>
+                    <div>
+                        <h1 className="text-amber-800" >Contato</h1>
+                        <br />
+                        <p>Fale Conosco</p>
+                        <p>Agendar Horario</p>
+                        <p>77777777777</p>
+                        <p>77777777777</p>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-700 container text-white mx-auto flex flex-col lg:flex-row justify-between items-center">
+                <div className="w-full p-1 lg:w-1/3 flex justify-center items-center">
+                    <p>Herika Darling</p>
+                </div>
+                <div className="w-full p-1 lg:w-1/3 flex justify-center items-center">
+                    <p>Conheça nossos termos de uso e políticas</p>
+                </div>
+                <div className="w-full p-1 lg:w-1/3 flex justify-center items-center">
+                    <p>Desenvolvido por ****</p>
+                </div>
+            </div>
+
+
+
+        </footer>
+
+    );
+};
+
+export default Footer;

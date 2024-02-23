0f791c0eceeac89a261371d5970ff807135a849e
ronaldo-oliv <124402134+oliveira-ronaldo@users.noreply.github.com>
2024-02-23 16:18:32 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Contact} from '@/components/layout/Navibar_contact'
 import {Menu} from '@/components/layout/Navibar_menu'
+import { Sobre_nos } from './components/layout/sobre_nos'
 export function App() {
   return (
     <div>
       <Contact/>
       <Menu/>
+      <Sobre_nos/>
     </div>
   )
 }
---
+++ src/components/layout/sobre_nos.tsx
@@ -0,0 +1,54 @@
+export const Sobre_nos = () => {
+    return (
+      <article className="flex">
+        <div className="w-1/2">
+          <div className="conteudo">
+            <h1>HERIKA DARLIGN espaço de beleza</h1>
+            <h2>Sobre o nosso espaço</h2>
+            <p>
+              O nosso espaço foi inaugurado em 2023, porém o nosso projeto já vem de
+              anos. Apenas no ano de 2023 ele ganhou forma e a colaboração de novas
+              profissionais. Hoje o nosso espaço é referência para Comercinho e toda
+              região
+            </p>
+            <div className="service">
+              <h3>Micropigmentação</h3>
+              <p>
+              A micropigmentação é uma técnica que utiliza um aparelho com agulhas e corrente elétrica para preencher sobrancelhas ou pigmentar os lábios. Na micropigmentação de sobrancelhas, fios são desenhados individualmente para criar um aspecto mais natural. Já na pigmentação labial, pigmentos são aplicados nos lábios para alterar o tom da pele, definir o contorno, corrigir assimetrias e disfarçar sinais de envelhecimento.
+              </p>
+            </div>
+            <div className="service1">
+              <h3>Limpeza de pele</h3>
+              <p>
+                A limpeza de pele é um procedimento estético e tem como principal
+                objetivo remover impurezas, células mortas, cravos, milium e
+                execesso de oleosidade da pele.
+              </p>
+            </div>
+            <div className="service2">
+              <h3>Unhas em fibra de vidro</h3>
+              <p>
+                As unhas em fibras de vidro são extremamente elegantes, proporcionam
+                maior praticidade no dia a dia, possui alta durabilidade e custo
+                benefício, além de elevar a autoestima da mulher.
+              </p>
+            </div>
+            <div className="service3">
+              <h3>Botox</h3>
+              <p>
+                O tratamento com a toxina botulínica previne rugas, possuí
+                resultados rápidos, suaviza marcas de expressão, pode ser aplicado
+                em várias áreas e evita o surgimento de rugas a longo prazo.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <div className="right-section">
+            {/* Aqui você pode adicionar a imagem */}
+          </div>
+        </div>
+      </article>
+    );
+  };
+  

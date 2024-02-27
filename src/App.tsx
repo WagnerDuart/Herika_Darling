6c35ab451d331dfaebdb1213296a82611992a06f
WagnerDuart <124386335+WagnerDuart@users.noreply.github.com>
2024-02-27 00:46:21 -0300

    configuracoes de deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import {Contact} from '@/components/layout/Navibar_contact'
-import {Menu} from '@/components/layout/Navibar_menu'
-import { Sobre_nos } from './components/layout/sobre_nos'
-import {Servicos} from '@/components/layout/servicos'
-import {Footer} from '@/components/layout/footer'
-import { Tela } from './components/layout/tela'
-import { Profissionais } from './components/layout/profissionais'
+// import {Contact} from '@/components/layout/Navibar_contact'
+// import {Menu} from '@/components/layout/Navibar_menu'
+// import { Sobre_nos } from './components/layout/sobre_nos'
+// import {Servicos} from '@/components/layout/servicos'
+// import {Footer} from '@/components/layout/footer'
+// import { Tela } from './components/layout/tela'
+// import { Profissionais } from './components/layout/profissionais'
+
+import { HomePage } from "./pages/home";
 
 export function App() {
+
   return (
+    
     <div>
-      <Contact/>
-      <Menu/>
-      <Tela/>
-      <Sobre_nos/>
-      <Servicos/>
-      <Profissionais/>
-      <Footer/>
+      <HomePage/>
     </div>
   )
 }
